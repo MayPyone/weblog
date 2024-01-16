061d51b2bf4060feea1ae68d71c3c9537728343d
MayPyone <maypyone015@gmail.com>
2024-01-16 10:42:49 +0630

    Add controller test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns http success' do
+      user = User.create(name: 'May Pyone  ', id: '1')
+      post = Post.create(author: user, title: 'Hello', text: 'This is my first post')
+      get user_posts_path(user)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      user = User.create(name: 'May Pyone  ', id: '1')
+      post = Post.create(author: user, title: 'Hello', text: 'This is my first post')
+      get user_post_path(user, post)
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'Elements for posts' do
+    it 'render the template of post index' do
+      user = User.create(name: 'May Pyone  ', id: '1')
+      post = Post.create(author: user, title: 'Hello', text: 'This is my first post')
+      get user_posts_path(user)
+      expect(response.body).to render_template('index')
+      expect(response.body).to include('<h1>Show all posts of a user</h1>')
+    end
+
+    it 'render the show template for post' do
+      user = User.create(name: 'May Pyone  ', id: '1')
+      post = Post.create(author: user, title: 'Hello', text: 'This is my first post')
+      get user_post_path(user, post)
+      expect(response.body).to render_template('show')
+      expect(response.body).to include('<h1>Show individual post of a user</h1>')
+    end
   end
 end
