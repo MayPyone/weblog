10221edeb39d96cef7fc8e7f1c84dd23088dc341
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-22 12:50:41 +0200

    Add test case that returns profile photo

--- spec/views/user_show_spec.rb
+++ spec/views/user_show_spec.rb
@@ -13,4 +13,13 @@ RSpec.describe 'Users', type: :feature do
     Post.create!(title: 'Intro', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user1)
     @post1 = Post.create!(title: 'Intro2', text: 'Hi', comment_counter: 0, like_counter: 2, author: @user2)
   end
+
+  describe 'returns user index page' do
+    it "returns users' profile" do
+      visit user_path(@user1)
+      expect(page).to have_css("img[src*='#{@user1.photo}']")
+      visit user_path(@user2)
+      expect(page).to have_css("img[src*='#{@user2.photo}']")
+    end
+  end
 end
